3c2edfbb7605ce1b78729f7d0d2dfecd5a8ed2e0
Ludvig Viveland <ludvigvive@gmail.com>
2020-10-27 12:36:52 +0100

    skriva in personnumer, läsa av och kontrollera antal siffror

--- Personnummer/Program.cs
+++ Personnummer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace PersonalCodeNumber
 {
@@ -11,15 +12,24 @@ namespace PersonalCodeNumber
         static void Main(string[] args)
         {
             // Declare variables
-
-            // Ask for personal code number, 12 digits
+            long personalCodeNumber;
+            string userInput;
+            bool isAPersonalCodeNumber;
+            
+            // Ask for personal code number, 12 digits (swedish)
+            Console.WriteLine("Skriv in ditt personnummer:");
 
             // Read input
-
+            userInput = Console.ReadLine();
+            personalCodeNumber = long.Parse(userInput);
             // Method, control right amount of digits
-
+            isAPersonalCodeNumber = ControlTwelveDigits(personalCodeNumber);
+            Console.WriteLine("det är 12 siffor");
             // Method, control right year, 1753-2020
-
+            //if (isAPersonalCodeNumber)
+            //{
+            //    isAPersonalCodeNumber = ControlRightYear(personalCodeNumber);
+            //}
             // Method, control month, 1-12
 
             // Method, leap year
@@ -30,7 +40,25 @@ namespace PersonalCodeNumber
 
             // Method, check sex
 
-            // Print if correct or not. (Assuming are all digits)
+            // Print if correct or not. (Assuming all are digits) (swedish)
+
+            //stop
+            Console.ReadKey();
         }
+
+        static bool ControlTwelveDigits(long PersonalCodeNumber)
+        {
+
+            if (Math.Log10(PersonalCodeNumber) == 12)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        //static bool ControlRightYear(int PersonalCodeNumber)
+        //{
+
+        //}
     }
 }
